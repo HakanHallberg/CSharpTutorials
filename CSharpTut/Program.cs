8819e561b99ede6f76eb31a88f80611a1951c66b
Hakan Hallberg <trixx.hkh@gmail.com>
2017-10-18 16:12:25 +0200

    Add project files.

---
+++ CSharpTut/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpTut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
