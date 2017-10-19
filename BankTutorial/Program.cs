bd8d1b25475adce387ccc5542005644e810020ba
Hakan Hallberg <trixx.hkh@gmail.com>
2017-10-19 13:28:35 +0200

    Created simple ATM. Created file for simple code exaples.

---
+++ BankTutorial/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace BankTutorial
+{
+    class Program
+    {
+        public class Account
+        {
+            public double balance;
+
+            public Account()
+            {
+                balance = 500.00;
+            }
+
+            public double Deposite()
+            {
+                double NewBalance;
+                double Deposite;
+                Console.WriteLine("Vänligen skriv belopp att sätta in.");
+                Deposite = double.Parse(Console.ReadLine());
+                NewBalance = balance + Deposite;
+                return NewBalance;
+            }  
+
+            public double Withdrawl()
+            {
+                double NewBalance;
+                double Withdrawl;
+                Console.WriteLine("Vänligen skriv belopp att ta ut.");
+                Withdrawl = double.Parse(Console.ReadLine());
+                NewBalance = balance - Withdrawl;
+                return NewBalance;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            double NewBalance;
+            string balance;
+            Account bank = new Account();
+            Console.WriteLine("Belopp på kontot: 500.00");
+            Console.WriteLine("Tryck 'i' för insättning eller 'u' för uttag.");
+
+            balance = Console.ReadLine();
+
+            if(balance == "i")
+            {
+                NewBalance = bank.Deposite();
+                Console.WriteLine("Belopp på konto efter insättning: {0} ", NewBalance);
+            }
+            if(balance == "u")
+            {
+                NewBalance = bank.Withdrawl();
+                Console.WriteLine("Belopp på konto efter uttag: {0} ", NewBalance);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
--- CSharpTut/Program.cs
+++ CSharpTut/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
 
 namespace CSharpTut
 {
@@ -6,7 +11,33 @@ namespace CSharpTut
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("Hello World!");
+
+            for (int i  = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("Arg {0} : {1}", i, args[i]);
+            }
+
+            string[] myArgs = Environment.GetCommandLineArgs();
+            Console.WriteLine(string.Join(", ", myArgs));
+
+            SayHello();
+            */
+
+
+
+            Console.ReadLine();
         }
-    }
+
+       /* private static void SayHello()
+        {
+            string name = "";
+            Console.Write("What is your name : ");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello {0}", name);
+
+        }
+        */
+    }  
 }
